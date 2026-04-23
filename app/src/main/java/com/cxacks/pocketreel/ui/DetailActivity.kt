5c65a263db88f0abae5474ca6a02e9ae80201e14
CaptainXack <captain@pocketreel.com>
2026-04-23 10:37:42 +0100

    Fix: Restored Manifest and Activity source files

---
+++ app/src/main/java/com/cxacks/pocketreel/ui/DetailActivity.kt
@@ -0,0 +1,10 @@
+package com.cxacks.pocketreel.ui
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // This is where the autoplay trailer lives
+    }
+}
